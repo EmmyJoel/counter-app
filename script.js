fd1f3ac7ca885ea0cac01b441992fa6855924bfe
EmmyJoel <emmanueloje30@gmail.com>
2022-08-31 01:05:47 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const numberText = document.querySelector('#counts')
+const previousEntry = document.querySelector('.data')
+
+
+let counts = 0;
+
+function update() {
+    numberText.textContent = counts;
+}
+
+function clear() {
+    counts = 0
+    update()
+}
+
+function increment() {
+    counts += 1
+    update()
+}
+
+function Delete() {
+    if (counts > 0) {
+        counts -= 1
+        update()
+    }
+}
+
+function save() {
+    let data =  numberText.textContent + ' + ';
+    if(numberText.textContent > 0) {
+        previousEntry.textContent +=  data;
+    }
+    clear()
+}
